26850b6d306f5a979b08b548f70bddbb2eccb54f
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-20 11:52:18 -0400

    Created category, payment, products & users routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const router = express.Router();
-// const usersCtrl = require('../../controllers/api/users');
-// const ensureLoggedIn = require('../../config/ensureLoggedIn');
-// const auth = require('../...config/auth')
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+const auth = require('../...config/auth')
 
-// router.post('/register', usersCtrl.register)
-// router.post('/login', usersCtrl.login)
-// router.get('/logout', usersCtrl.logout)
-// router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
-// router.patch('/addKart', auth, usersCtrl.addKart)
-// router.get('/info', auth,  usersCtrl.getUser)
-// router.get('/history', auth, usersCtrl.history)
+router.post('/register', usersCtrl.register)
+router.post('/login', usersCtrl.login)
+router.get('/logout', usersCtrl.logout)
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
+router.patch('/addKart', auth, usersCtrl.addKart)
+router.get('/info', auth,  usersCtrl.getUser)
+router.get('/history', auth, usersCtrl.history)
 
-// module.exports = router
+module.exports = router
