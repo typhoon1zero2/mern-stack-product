b30221c22739c29747526ec4837fbba0ff4346cf
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-20 13:27:19 -0400

    Created users controller

--- controllers/api/usersCtrl.js
+++ controllers/api/usersCtrl.js
@@ -1,4 +1,4 @@
-const User = require('../../models/User');
+const User = require('../../models/Users');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
  /*************************************************************
   *     Put API routes here, before the "catch all" route
   **************************************************************/
- app.use('./api/users', require('./routes/api/users'));
+ //app.use('./api/users', require('./routes/api/users'));
 
  /*************************************************************
   *   The following "catch all" route (note the *) is necessary
