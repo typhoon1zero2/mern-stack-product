d57d1901b54d7167878ba58fb8d251e49e0833f6
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-21 16:37:14 -0400

    created routers

--- routes/api/usersRoute.js
+++ routes/api/usersRoute.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
-const ensureLoggedIn = require('../../config/ensureLoggedIn');
-const auth = require('../...config/auth')
+const usersCtrl = require('../../controllers/api/usersCtrl');
+// const ensureLoggedIn = require('../../config/ensureLoggedIn');
+const auth = require('../../config/auth')
 
+//test router
+//  router.post('/register', (req, res) =>{
+//      res.json({ msg: "test router"})
+//  })
+//test controller
 router.post('/register', usersCtrl.register)
+router.get('/refresh_token', usersCtrl.refreshToken)
 router.post('/login', usersCtrl.login)
 router.get('/logout', usersCtrl.logout)
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
-router.patch('/addKart', auth, usersCtrl.addKart)
-router.get('/info', auth,  usersCtrl.getUser)
-router.get('/history', auth, usersCtrl.history)
+router.get('/check-token', usersCtrl.checkToken)
+// router.patch('/addKart', auth, usersCtrl.addKart)
+router.get('/info', auth ,  usersCtrl.getUser)
+// router.get('/history', auth, usersCtrl.history)
 
 module.exports = router
