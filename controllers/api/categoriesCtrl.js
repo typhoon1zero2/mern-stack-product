a6fe0f748bf0c60f3a900e8b2e35fa256b048425
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-21 18:07:33 -0400

    Created read category controllers function

--- controllers/api/categoriesCtrl.js
+++ controllers/api/categoriesCtrl.js
@@ -1,6 +1,21 @@
+const Category = require('../../models/Category');
+
+
+
+/********************************************
+ * CREATE -READ - UPDATE - DELETE
+ *******************************************/
 const categoryCtrl = {
+    getCategories: async(req, res) =>{
+        //res.json('Category Test Ctrl')
+        try{
+            const categories = await Category.find()
+            res.json(categories)
 
+        }catch (err) {
+            return res.status(200).json({ msg: err.massage })
+        }
+    }
 }
-
 
 module.exports = categoryCtrl;
