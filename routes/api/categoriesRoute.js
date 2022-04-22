24a37fb61750d80d2a09113cdf6d57ae82933237
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-22 13:42:55 -0400

    Updated category route Update,Create, Deleted

--- routes/api/categoriesRoute.js
+++ routes/api/categoriesRoute.js
@@ -1,14 +1,19 @@
 // /routes/api/users.js
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router()
+//const router = express.Router();
 const categoryCtrl = require('../../controllers/api/categoriesCtrl');
 const auth = require('../../config/auth.js');
 const authAdmin = require('../../config/authAdmin.js')
 
+// POST /api/users/
+
 router.route('/category')
     .get(categoryCtrl.getCategories)
-    
+    //.post(auth, authAdmin, categoryCtrl.createCategory)
+    .post(categoryCtrl.createCategory)
 
-
+router.route('/category/:id')
+    .delete(categoryCtrl.deleteCategory)
+    .put(categoryCtrl.updateCategory)
 
 module.exports = router;
