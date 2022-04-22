8c6ce58c85d96bbbb2c0fe20848a1f8fd9b5d504
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-22 11:18:42 -0400

    Created get routes & controller

--- routes/api/categoriesRoute.js
+++ routes/api/categoriesRoute.js
@@ -1,8 +1,14 @@
-const router = require('express').Router()
+// /routes/api/users.js
+const express = require('express');
+const router = express.Router();
+const categoryCtrl = require('../../controllers/api/categoriesCtrl');
+const auth = require('../../config/auth.js');
+const authAdmin = require('../../config/authAdmin.js')
 
 router.route('/category')
+    .get(categoryCtrl.getCategories)
     
-    
+
 
 
 module.exports = router;
