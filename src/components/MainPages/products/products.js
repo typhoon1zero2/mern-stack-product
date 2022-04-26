352114839f54b29bd92e6f7a6e4f1765505886ff
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-26 12:43:29 -0400

    Created products function

--- src/components/MainPages/products/products.js
+++ src/components/MainPages/products/products.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalState } from "../../../GlobalState";
+import ProductItem from '../../MainPages/utilities/productsItem/ProductItem'
 
-function products() {
-  return (
-    <div>products list</div>
-  )
+
+
+export default function Products() {
+  const state = useContext(GlobalState);
+
+  const [products] = state.productsAPI.products;
+  //console.log(products)
+  return <div className="products">
+    {
+      products.map(products =>{
+        return <ProductItem key={products._id} product={products} />
+      })
+    }
+  </div>;
 }
-
-export default products
