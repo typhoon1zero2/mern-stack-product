40de0d992e23ca013ce2a73ad9ad6ccf0e1b435c
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-25 23:42:31 -0400

    Imported files

---
+++ src/components/MainPages/Pages.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Products from './products/products';
+import Login from './auth/LoginForm/LoginForm';
+import Signup from './auth/SignUpForm/SignUpForm';
+import Cart from './cart/cart'
+
+function Pages() {
+  return (
+    <div>
+        Main Pages components
+    </div>
+  )
+}
+
+export default Pages
