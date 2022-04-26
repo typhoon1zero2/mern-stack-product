38c60fd40d1716b1cec75a3a8100995dc6e96cf3
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-25 23:15:10 -0400

    Created header and Style css

--- src/components/Headers/header.js
+++ src/components/Headers/header.js
@@ -3,6 +3,8 @@ import { GlobalState } from "../../GlobalState";
 import Menu from "../Headers/icons/menu.svg";
 import Close from "../Headers/icons/close.svg";
 import Cart from "../Headers/icons/cart.svg";
+import { Link } from "react-router-dom";
+
 function Header() {
   const value = useContext(GlobalState);
   return (
@@ -11,10 +13,25 @@ function Header() {
         <img src={Menu} alt="" width="30" />
       </div>
       <div className="logo">
-        <img src={Close} alt="" width="30" className="menu" />
+        <h1>
+          <Link to="/">Products Store</Link>
+        </h1>
       </div>
+
+      <ul>
+        <li>
+          <Link to="/">Products</Link>
+        </li>
+        <li>
+          <Link to="/login">Login ✥ Register</Link>
+        </li>
+        <li>
+          <img src={Close} alt="" width="30" className="menu" />
+        </li>
+      </ul>
       <div className="cart-icon">
-        <img src={Cart} alt="" width="30" />
+        <span>0</span>
+        <Link to="/cart"> <img src={Cart} alt="" width="30" /></Link>
       </div>
     </header>
   );
