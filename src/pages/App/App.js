28b45ad4aa5a4001772f1d94339f49803f660fe1
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-25 23:13:54 -0400

    Created main pages

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
-import {DataProvider} from './GlobalState'
-import Header from './components/headers/Header'
-import MainPages from './components/MainPages/Pages'
+import {DataProvider} from '../../GlobalState'
+import Header from '../../components/Headers/header';
+import MainPages from '../../components/MainPages/Pages';
 
-
-function App() {
+export default function App() {
+ 
   return (
     <DataProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <MainPages />
-        </div>
-      </Router>
-    </DataProvider>
+    <Router>
+      <div className="App">
+        <Header />
+        <MainPages />
+      </div>
+    </Router>
+  </DataProvider>
   );
 }
-
-export default App;
