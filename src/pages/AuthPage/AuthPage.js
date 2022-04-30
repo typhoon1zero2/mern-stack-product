0571938008e4187d00d714e3e102b1f799e966fe
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-30 16:34:54 -0400

    Minor changes & css

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,18 +1,17 @@
 import SignUpForm from "../../components/MainPages/auth/SignUpForm/SignUpForm";
 import LoginForm from "../../components/MainPages/auth/LoginForm/LoginForm";
 import { useState } from "react";
-// import Logo from "../../components/Logo/Logo"
-
+import {GiWorld} from "react-icons/gi"
 export default function AuthPage({ setUser, showLogin, setShowLogin }) {
   return (
+    
     <main className="auth-page">
-      <div>
-        <h1><em>Hello</em> World!</h1>
-        <br />
-        <p>
-          Welcome to my <em>ty</em>Phoong Store!
-        </p>
-      </div>
+     <br />
+      <h1 className="logo"> <GiWorld /></h1>
+        <h2 className="login">
+          Login || Register
+        </h2>
+
       {/* <Logo /> */}
       {showLogin ? (
         <LoginForm setUser={setUser} />
