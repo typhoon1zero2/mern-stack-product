ae11aa6cb2e4e89521d3d9e19f9c48d956b47e5e
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-02 22:40:14 -0400

    Created fs unlink method

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,51 +1,52 @@
-const router = require('express').Router()
-const cloudinary = require('cloudinary')
+const router = require("express").Router();
+const cloudinary = require("cloudinary");
 
-const fs = require('fs')
-
+const fs = require("fs");
 
 // we will upload image on cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 // Upload image only admin can use
-router.post('/upload', (req, res) =>{
-    try {
-        console.log(req.files)
+router.post("/upload", (req, res) => {
+  try {
+    console.log(req.files);
 
-        if(!req.files || Object.keys(req.files).length === 0)
-            return res.status(200).json({ msg: 'No files were uploaded.' })
-            const file = req.files.file;
-            if(file.size > 1024*1024) {
-                
-                return res.status(200).json({ msg: "Size too large" })
-            }
-
-        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
-            
-            return res.status(200).json({ msg: "File format is incorrect." })
-        }
-        cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "test"}, async(err, result)=>{
-            if(err) throw err;
-            res.json({public_id: result.public_id, url: result.secure_url})           
-
-            
-        })
-
-
-
-    } catch (err) {
-        return res.status(400).json({msg: err.message})
+    if (!req.files || Object.keys(req.files).length === 0)
+      return res.status(200).json({ msg: "No files were uploaded." });
+    const file = req.files.file;
+    if (file.size > 1024 * 1024) {
+      removeTmp(file.tempFilePath);
+      return res.status(200).json({ msg: "Size too large" });
     }
-})
 
-const removeTmp = (path) =>{
-    fs.unlink(path, err=>{
-        if(err) throw err;
-    })
-}
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+      removeTmp(file.tempFilePath);
+      return res.status(200).json({ msg: "File format is incorrect." });
+    }
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: "test" },
+      async (err, result) => {
+        if (err) throw err;
+        removeTmp(file.tempFilePath);
+        res.json({ public_id: result.public_id, url: result.secure_url });
+      }
+    );
+  } catch (err) {
+    return res.status(400).json({ msg: err.message });
+  }
+});
 
-module.exports = router
+/* The fs.unlink() method is used to remove a file or symbolic link from the filesystem. This function does not work on directories, therefore it is recommended to use fs.rmdir() to remove a directory.*/
+
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
+};
+
+module.exports = router;
