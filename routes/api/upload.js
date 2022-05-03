a9a9314416593133582e5458abf8738600fcd374
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-02 22:35:34 -0400

    Created router post upload picture

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -14,15 +14,38 @@ cloudinary.config({
 // Upload image only admin can use
 router.post('/upload', (req, res) =>{
     try {
+        console.log(req.files)
 
-console.log(req.files)
-res.json('test upload')
+        if(!req.files || Object.keys(req.files).length === 0)
+            return res.status(200).json({ msg: 'No files were uploaded.' })
+            const file = req.files.file;
+            if(file.size > 1024*1024) {
+                
+                return res.status(200).json({ msg: "Size too large" })
+            }
+
+        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+            
+            return res.status(200).json({ msg: "File format is incorrect." })
+        }
+        cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "test"}, async(err, result)=>{
+            if(err) throw err;
+            res.json({public_id: result.public_id, url: result.secure_url})           
+
+            
+        })
+
+
 
     } catch (err) {
         return res.status(400).json({msg: err.message})
     }
 })
 
-
+const removeTmp = (path) =>{
+    fs.unlink(path, err=>{
+        if(err) throw err;
+    })
+}
 
 module.exports = router
