dd5b8074ccd2163e183bd40cf93c6cdb14c80c2d
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-07 15:58:30 -0400

    Make minor change to deploy heroku

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api', require('./routes/api/upload'))
 // Protect the API routes below from anonymous users
 const ensureLoggedIn = require('./config/ensureLoggedIn');
 app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
-app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
+// app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
 
 
 // The following "catch all" route (note the *) is necessary
