ddcb79c3e573c01e2e4769d74f0cb64df8672a30
ptnguyen617 <ptnguyen617@yahoo.com>
2022-05-09 16:28:31 -0400

    Make it better

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -15,11 +15,13 @@ export default function Footer(props) {
               fontSize: "2em",
             }}
           >
-           <em>Japanese</em> Classic Cars
+            <em>Japanese</em> Classic Cars
           </h3>
-          <p className="phong">Japanese Classic Cars is a registered company under typhoong LLC.</p>
+          <p className="phong">
+            Japanese Classic Cars is a registered company under typhoong LLC.
+          </p>
           <br />
-          <div className="sub">
+          {/* <div className="sub">
             <div>
               <b>Company</b>
               <p>About</p>
@@ -41,43 +43,42 @@ export default function Footer(props) {
               <p>Security</p>
               <p>Terms</p>
             </div>
+            <div> */}
+          <div className="contact">
+            <b>Contacts links</b>
             <div>
-              <b>Contacts links</b>
-              <div>
-               
-                  <h2>
-                    {" "}
-                    <a href="https://github.com/typhoon1zero2">
-                      github
-                    </a>
-                  </h2>
-               
-               
-                  <h2>
-                    {" "}
-                    <a
-                      class="link-secondary"
-                      href="mailto:ptnguyen617@yahoo.com"
-                    >
-                      mailto
-                    </a>
-                  </h2>
-               
-               
-                  <h2>
-                    <a
-                      class="link-secondary"
-                      href="https://www.linkedin.com/in/phong-nguyen-b9520b22b/"
-                    >
-                     linkedin
-                    </a>
-                  </h2>
-              
-              </div>
+              {" "}
+              <a href="https://github.com/typhoon1zero2">
+                {" "}
+                <img
+                  className="github"
+                  src="https://res.cloudinary.com/deeztyphoon0529/image/upload/v1652127527/test/i4unfpzxx0mhgednyttd.png"
+                  alt="github"
+                />
+              </a>{" "}
+              <a class="link-secondary" href="mailto:ptnguyen617@yahoo.com">
+                <img
+                  className="mail"
+                  src="https://res.cloudinary.com/deeztyphoon0529/image/upload/v1652127488/test/dause8nwqqgygjiok4sn.png"
+                  alt="mail"
+                />
+              </a>
+              <a
+                class="link-secondary"
+                href="https://www.linkedin.com/in/phong-nguyen-b9520b22b/"
+              >
+                <img
+                  className="linked"
+                  src="https://res.cloudinary.com/deeztyphoon0529/image/upload/v1652127209/test/tgzlfuocrpowuahiix3a.png"
+                  alt="linked"
+                />
+              </a>
             </div>
           </div>
         </div>
       </div>
     </div>
+    //   </div>
+    // </div>
   );
 }
